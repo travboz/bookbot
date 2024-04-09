826d093664e418c60bfd246755396184f9de6535
Travis Bozic <z5368318@ad.unsw.edu.au>
2024-04-09 11:17:13 +1000

    Add character frequency counting function

--- main.py
+++ main.py
@@ -4,14 +4,30 @@ def get_book_text(book_path):
 
 
 def count_words(text):
-    return len(text.split())
+    words = text.split()
+    return len(words)
+
+
+def count_letter_frequency(text):
+    char_counts = {}
+
+    for ch in text.lower():
+        if ch not in char_counts:
+            char_counts[ch] = 1
+        else:
+            char_counts[ch] += 1
+
+    return char_counts
 
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     text_word_count = count_words(text)
-    print(text_word_count)
+    character_frequencies = count_letter_frequency(text)
+
+    print(f"The number of words in the text: {text_word_count}")
+    print(f"The frequencies of each character in the text: {character_frequencies}")
 
 
 if __name__ == "__main__":
