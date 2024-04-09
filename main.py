f7ece462a0ef59e168ac14e6e7339b52c988a2ff
Travis Bozic <z5368318@ad.unsw.edu.au>
2024-04-09 12:01:39 +1000

    Create main functions plus helpers to generate a report summarising the text input.

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 def get_book_text(book_path):
+    """Get the text content of a file returned as a string"""
     with open(book_path) as f:
         return f.read()
 
 
 def count_words(text):
+    """Count the words in text"""
     words = text.split()
     return len(words)
 
 
 def count_letter_frequency(text):
+    """Calculate the frequency of characters in a text"""
     char_counts = {}
 
     for ch in text.lower():
@@ -20,37 +23,42 @@ def count_letter_frequency(text):
     return char_counts
 
 
+# callback function to sort by - sorting by the count of characters
 def sort_on_value(dict):
-    return dict["num"]
+    """Callback to sort by the count value of the dict"""
+    return dict["count"]
+
+
+def sort_chars_counts(cdict):
+    """Sort the character dictionary in descending order by value"""
+    chars = []
+    for char, freq in cdict.items():
+        if char.isalpha():
+            chars.append({"char": char, "count": freq})
+
+    chars.sort(reverse=True, key=sort_on_value)
+    return chars
 
 
 def generate_report(book_path):
-    book_path = "books/frankenstein.txt"
-
+    """Generate a highlight report of a book"""
     text = get_book_text(book_path)
     text_word_count = count_words(text)
     character_frequencies = count_letter_frequency(text)
-
-    character_frequencies.sort(reverse=True, key=sort_on_value)
+    sorted = sort_chars_counts(character_frequencies)
 
     print(f"--- Begin report of {book_path} ---")
-
     print(f"{text_word_count} words found in the document\n")
 
-    for k, v in character_frequencies.items():
-        print(f"The '{k}' character was found {v} times")
+    for info in sorted:
+        print(f"The '{info["char"]}' character was found {info["count"]} times")
 
     print("--- End report ---")
 
 
 def main():
     book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    text_word_count = count_words(text)
-    character_frequencies = count_letter_frequency(text)
-
-    print(f"The number of words in the text: {text_word_count}")
-    print(f"The frequencies of each character in the text: {character_frequencies}")
+    generate_report(book_path)
 
 
 if __name__ == "__main__":
